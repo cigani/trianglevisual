098019b037eadd94c8998d1f2525d050adcd8ac1
mjaquier <t>
2020-09-25 10:38:14 +0200

    Propery checking

--- nrkvisual/visual/plotting.py
+++ nrkvisual/visual/plotting.py
@@ -1,3 +1,4 @@
+import operator
 from functools import reduce, partial
 
 import matplotlib.pyplot as plt
@@ -7,43 +8,100 @@ from pandas import DataFrame, Series
 
 
 class TrianglePlot:
-    def __init__(self, data: DataFrame, data_column: str, tick=0.1, margin=0.05, n=5):
+    ERROR_CODES = {
+        "NA": lambda x: f"Found empty data here {x}",
+        "EMPTY": "We found no data",
+        "SHAPE": lambda x: f"Expected at least 4 columns (3 Axes 1 Data + Optional Temperature) we found only the following columns in the data provided {x}",
+        "DATA": lambda x: f"We couldn't find your data column in the provided data you provided {x or ' < NONE >'} as the data column"
+    }
+    errors = []
+    critical = False
 
+    def __init__(self, data: DataFrame, data_column: str, tick=0.1, margin=0.05, n=5):
         self.data = data
+        self.data_column = data_column
+
         self.a = Series
         self.b = Series
         self.c = Series
         self.v = Series
         self.x = Series
         self.y = Series
+        self.axes = DataFrame
         self.tick = tick
         self.margin = margin
         self.n = n
         self.T = None
-        self.v = data.rename(columns=lambda x: x.lower())[data_column.lower()]
-        self.axes = data.rename(columns=lambda x: x.lower()).drop(columns=[data_column.lower()])
+
         self.fig, self.ax = plt.subplots(1)
         self.ax.set_axis_off()
 
-    def plot(self):
-        self.cords()
-        self.triangle()
-        self.set_up()
-        plt.show()
+    @property
+    def data_column(self):
+        return self._data_column
+
+    @property
+    def data(self):
+        return self._data
+
+    @data_column.setter
+    def data_column(self, value: str):
+        value = value.strip().lower()
+        if value not in self.data.rename(columns=lambda x: x.strip().lower()):
+            self.errors.clear()
+            self.errors.append(self.ERROR_CODES['DATA'](value))
+            self.critical = True
+        self._data_column = value
+
+    @data.setter
+    def data(self, d: DataFrame):
+        if d.empty:
+            self.errors.clear()
+            self.errors.append(self.ERROR_CODES["EMPTY"])
+            self.critical = True
+        if d.isnull().values.any():
+            self.errors.append(
+                self.ERROR_CODES["NA"](d[d.isnull().any(axis=1)].index.values)
+            )
+            d = d.dropna()
+        if d.shape[1] < 4:
+            self.errors.clear()
+            self.errors.append(self.ERROR_CODES["SHAPE"](d.columns.values))
+
+            self.critical = True
+        self._data = d
 
     @staticmethod
     def chain(function, series):
         mFunction = partial(function)
         return reduce(mFunction, series)
 
-    def cords(self):
+    def plot(self):
+        if not self.critical:
+            self.xy()
+            self.cords()
+            self.triangle()
+            self.set_up()
+            plt.show()
+        else:
+            return self.errors
+
+    def xy(self):
+        self.v = self.data.rename(columns=lambda x: x.lower())[self.data_column.lower()]
+        self.axes = self.data.rename(columns=lambda x: x.lower()).drop(
+            columns=[self.data_column.lower()]
+        )
         self.axes = self.normalize(self.axes)
+
+    def cords(self):
+
         self.a = self.axes[self.axes.columns[0]]
         self.b = self.axes[self.axes.columns[1]]
         self.c = self.axes[self.axes.columns[2]]
 
-    def normalize(self, df):
-        if (df.sum(axis=1) > 1.5).any(): # Accounting for input errors
+    @staticmethod
+    def normalize(df):
+        if (df.sum(axis=1) > 1.5).any():  # Accounting for input errors
             df = df.div(100)
         return df
 
@@ -68,9 +126,9 @@ class TrianglePlot:
         self.annotate(tn, [0.5, 1], [0.5, 1.09])
         self.annotate(ln, [0, 0], [-0.04, 0.02])
         self.annotate(rn, [1, 0], [1.04, 0.02])
-        self.annotate(f'{ln}-{rn}', [0.5, 0], [0.5, -0.09])
-        self.annotate(f'{rn}-{tn}', [0.72, 0.5], [0.9, 0.55])
-        self.annotate(f'{ln}-{tn}', [0.2, 0.5], [0.1, 0.55])
+        self.annotate(f"{ln}-{rn}", [0.5, 0], [0.5, -0.09])
+        self.annotate(f"{rn}-{tn}", [0.72, 0.5], [0.9, 0.55])
+        self.annotate(f"{ln}-{tn}", [0.2, 0.5], [0.1, 0.55])
         plt.tricontourf(self.x, self.y, self.T.triangles, self.v)
         corners = np.array([[0, 0], [1, 0], [0.5, np.sqrt(3) * 0.576]])
         triangle = tri.Triangulation(corners[:, 0], corners[:, 1])
@@ -81,7 +139,13 @@ class TrianglePlot:
         self.fig.tight_layout()
 
     def annotate(self, text, cord, textcord):
-        self.ax.annotate(text, xy=cord, xytext=textcord, horizontalalignment='center', verticalalignment='top')
+        self.ax.annotate(
+            text,
+            xy=cord,
+            xytext=textcord,
+            horizontalalignment="center",
+            verticalalignment="top",
+        )
 
     def plot_ticks(self, start, stop, tick, offset=(0.0, 0.0)):
         r = np.linspace(0.1, 0.9, self.n + 1)
